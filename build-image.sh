45d81589a03e752eaf4737f18d7c487c6593dadc
Damien DeVille <damien@steady.org>
2023-06-29 18:11:00 -0400

    bug fixes

--- build-image.sh
+++ build-image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-docker build -t 0xsteady/minionsparser-python:1.0 .
+docker build --no-cache -t 0xsteady/minionsparser-python:1.0 .
 docker run -itd -e TZ=America/New_York --mount type=bind,source=/tmp/minionsparser/,target=/tmp/minionsparser --name minionsparser-python 0xsteady/minionsparser-python:1.0
